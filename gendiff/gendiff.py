437f568426bb8018868ae4612954f25da01952a3
Schegolkova <broodingthinker@gmail.com>
2024-10-22 02:29:32 +0300

    rewrote  generate_diff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -7,42 +7,38 @@ def generate_diff(file_path1, file_path2, format_name='stylish'):
     file1 = parse(file_path1)
     file2 = parse(file_path2)
     def inner(data1, data2=None):
+            
+        if isinstance(data1, dict) and isinstance(data2, dict):
+            keys = data1.keys() | data2.keys()
+            result = {}
+            for key in keys:
+                if key not in data1:
+                    result[key] = {
+                        'status': 'added',
+                        'value': inner(data2[key])[0]
+                    }
+                elif key not in data2:
+                    result[key] = {
+                        'status': 'removed',
+                        'value': inner(data1[key])[0]
+                    }
+                elif isinstance(data2[key], dict):
+                    result[key] = inner(data1[key], data2[key])
+                elif data1[key] != data2[key]:
+                    result[key] = {
+                        'status': 'updated',
+                        'old value': inner(data1[key], data2[key])[0],
+                        'new value': inner(data1[key], data2[key])[1]
+                    }
+                else:
+                    result[key] = {
+                        'status': 'no change',
+                        'value': data1[key]
+                    }
 
-        if not isinstance(data1, dict) and isinstance(data2, dict):
+            return dict(sorted(result.items(), key=lambda x: x[0]))
+        else:
             return data1, data2
-        elif isinstance(data1, dict) and not isinstance(data2, dict):
-            return data1, data2
-        elif not isinstance(data1, dict) and not isinstance(data2, dict):
-            return data1, data2
-
-        keys = data1.keys() | data2.keys()
-        result = {}
-        for key in keys:
-            if key not in data1:
-                result[key] = {
-                    'status': 'added',
-                    'value': inner(data2[key])[0]
-                }
-            elif key not in data2:
-                result[key] = {
-                    'status': 'removed',
-                    'value': inner(data1[key])[0]
-                }
-            elif isinstance(data2[key], dict):
-                result[key] = inner(data1[key], data2[key])
-            elif data1[key] != data2[key]:
-                result[key] = {
-                    'status': 'updated',
-                    'old value': inner(data1[key], data2[key])[0],
-                    'new value': inner(data1[key], data2[key])[1]
-                }
-            else:
-                result[key] = {
-                    'status': 'no change',
-                    'value': data1[key]
-                }
-
-        return dict(sorted(result.items(), key=lambda x: x[0]))
 
     match format_name:
         case 'stylish':
