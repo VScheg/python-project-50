ef57b76730f8a5b00008cc439d3938174fc82779
Schegolkova <broodingthinker@gmail.com>
2024-10-22 11:18:24 +0300

    minor fixes

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -6,8 +6,9 @@ from gendiff.parsing import parse
 def generate_diff(file_path1, file_path2, format_name='stylish'):
     file1 = parse(file_path1)
     file2 = parse(file_path2)
+
     def inner(data1, data2=None):
-            
+
         if isinstance(data1, dict) and isinstance(data2, dict):
             keys = data1.keys() | data2.keys()
             result = {}
