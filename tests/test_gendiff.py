a1053bd6e9d23fd784a85a6b74f649d10ad2ff00
Schegolkova <broodingthinker@gmail.com>
2024-11-10 15:46:16 +0300

    updated test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,5 +1,7 @@
+import pytest
 import os
 from gendiff import generate_diff
+from gendiff.reading import read
 
 
 def get_fixture_path(file_name):
@@ -7,46 +9,61 @@ def get_fixture_path(file_name):
     return os.path.join(current_dir, 'fixtures', file_name)
 
 
-def read(file_path):
-    with open(file_path, 'r') as f:
-        result = f.read()
-    return result
+@pytest.mark.parametrize(
+        "file_name_1,file_name_2,format,expected",
+        [
+            (get_fixture_path('flat1.json'), get_fixture_path('flat2.json'), 'stylish', read(get_fixture_path('stylish_flat_result1.txt'))),
+            (get_fixture_path('flat1.yaml'), get_fixture_path('flat2.yml'), 'stylish', read(get_fixture_path('stylish_flat_result1.txt'))),
+            (get_fixture_path('flat1.json'), get_fixture_path('flat3.json'), 'stylish', read(get_fixture_path('stylish_flat_result2.txt')))
+    ],
+)
+def test_stylish_flat(file_name_1, file_name_2, format, expected):
+    assert generate_diff(file_name_1, file_name_2, format) == expected
 
 
-def test_stylish_flat():
-    expected = read(get_fixture_path('stylish_flat_result1.txt'))
-    assert generate_diff(get_fixture_path('flat1.json'), get_fixture_path('flat2.json')) == expected
-    assert generate_diff(get_fixture_path('flat1.yaml'), get_fixture_path('flat2.yml')) == expected
-
-    expected = read(get_fixture_path('stylish_flat_result2.txt'))
-    assert generate_diff(get_fixture_path('flat1.json'), get_fixture_path('flat3.json'), 'stylish') == expected
-
-
-def test_stylish_nested():
-    expected = read(get_fixture_path('stylish_nested_result1.txt'))
-    assert generate_diff(get_fixture_path('nested1.json'), get_fixture_path('nested2.json')) == expected
-    assert generate_diff(get_fixture_path('nested1.yml'), get_fixture_path('nested2.yml')) == expected
-    assert generate_diff(get_fixture_path('nested1.yml'), get_fixture_path('nested2.json')) == expected
-
-    expected = read(get_fixture_path('stylish_nested_result2.txt'))
-    assert generate_diff(get_fixture_path('nested1.json'), get_fixture_path('nested3.json')) == expected
+@pytest.mark.parametrize(
+        "file_name_1,file_name_2,expected",
+        [
+            (get_fixture_path('nested1.json'), get_fixture_path('nested2.json'), read(get_fixture_path('stylish_nested_result1.txt'))),
+            (get_fixture_path('nested1.yml'), get_fixture_path('nested2.yml'), read(get_fixture_path('stylish_nested_result1.txt'))),
+            (get_fixture_path('nested1.yml'), get_fixture_path('nested2.json'), read(get_fixture_path('stylish_nested_result1.txt'))),
+            (get_fixture_path('nested1.json'), get_fixture_path('nested3.json'), read(get_fixture_path('stylish_nested_result2.txt')))
+    ],
+)
+def test_stylish_nested(file_name_1, file_name_2, expected):
+    assert generate_diff(file_name_1, file_name_2) == expected
 
 
-def test_plain_flat():
-    expected = read(get_fixture_path('plain_flat_result.txt'))
-    assert generate_diff(get_fixture_path('flat1.json'), get_fixture_path('flat2.json'), 'plain') == expected
-    assert generate_diff(get_fixture_path('flat1.yaml'), get_fixture_path('flat2.yml'), 'plain') == expected
+@pytest.mark.parametrize(
+        "file_name_1,file_name_2,format,expected",
+        [
+            (get_fixture_path('flat1.json'), get_fixture_path('flat2.json'), 'plain', read(get_fixture_path('plain_flat_result.txt'))),
+            (get_fixture_path('flat1.yaml'), get_fixture_path('flat2.yml'), 'plain', read(get_fixture_path('plain_flat_result.txt'))),
+    ],
+)
+def test_plain_flat(file_name_1, file_name_2, format, expected):
+    assert generate_diff(file_name_1, file_name_2, format) == expected
 
 
-def test_plain_nested():
-    expected = read(get_fixture_path('plain_nested_result.txt'))
-    assert generate_diff(get_fixture_path('nested1.json'), get_fixture_path('nested2.json'), 'plain') == expected
-    assert generate_diff(get_fixture_path('nested1.yml'), get_fixture_path('nested2.yml'), 'plain') == expected
-    assert generate_diff(get_fixture_path('nested1.yml'), get_fixture_path('nested2.json'), 'plain') == expected
+@pytest.mark.parametrize(
+        "file_name_1,file_name_2,format,expected",
+        [
+            (get_fixture_path('nested1.json'), get_fixture_path('nested2.json'), 'plain', read(get_fixture_path('plain_nested_result.txt'))),
+            (get_fixture_path('nested1.yml'), get_fixture_path('nested2.yml'), 'plain', read(get_fixture_path('plain_nested_result.txt'))),
+            (get_fixture_path('nested1.yml'), get_fixture_path('nested2.json'), 'plain', read(get_fixture_path('plain_nested_result.txt'))),
+    ],
+)
+def test_plain_nested(file_name_1, file_name_2, format, expected):
+    assert generate_diff(file_name_1, file_name_2, format) == expected
 
 
-def test_json_nested():
-    expected = read(get_fixture_path('json_nested_result.txt'))
-    assert generate_diff(get_fixture_path('nested1.json'), get_fixture_path('nested2.json'), 'json') == expected
-    assert generate_diff(get_fixture_path('nested1.yml'), get_fixture_path('nested2.yml'), 'json') == expected
-    assert generate_diff(get_fixture_path('nested1.yml'), get_fixture_path('nested2.json'), 'json') == expected
+@pytest.mark.parametrize(
+        "file_name_1,file_name_2,format,expected",
+        [
+            (get_fixture_path('nested1.json'), get_fixture_path('nested2.json'), 'json', read(get_fixture_path('json_nested_result.txt'))),
+            (get_fixture_path('nested1.yml'), get_fixture_path('nested2.yml'), 'json', read(get_fixture_path('json_nested_result.txt'))),
+            (get_fixture_path('nested1.yml'), get_fixture_path('nested2.json'), 'json', read(get_fixture_path('json_nested_result.txt'))),
+    ],
+)
+def test_json_nested(file_name_1, file_name_2, format, expected):
+    assert generate_diff(file_name_1, file_name_2, format) == expected
